eebcf2d94fcfbfb906ebfde191c408363cff3813
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-21 18:00:55 -0600

    Add: Include our newly created sub-class within config.php to make it available to appropriate files Add: Database connection comments

--- inc/config.php
+++ inc/config.php
@@ -4,7 +4,9 @@ session_start();
 //include the user class, pass in the database connection
 include __DIR__ . '/../classes/Collection.php';
 include __DIR__ . '/../classes/ListingBasic.php';
+include __DIR__ . '/../classes/ListingPremium.php';
 
+//connect to the database
 try {
     //create PDO connection
     $db = new PDO("sqlite:".__DIR__."/database.db");
@@ -14,5 +16,6 @@ try {
     echo '<p class="bg-danger">'.$e->getMessage().'</p>';
     exit;
 }
+//create directory object passing in the connection to our collection class
 //load main collection object
 $directory = new Collection($db);
