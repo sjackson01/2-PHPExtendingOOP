27aa703b7e957190e6390005eae8fa026b715989
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-22 17:53:58 -0600

    Add: Navigation based on status default index.php

--- index.php
+++ index.php
@@ -21,6 +21,25 @@ var_dump(get_class($test));
 var_dump(is_a($test, 'ListingBasic'));
 var_dump($test->getStatus());
 */
+
+echo '<ul class="nav nav-tabs">';
+echo '<li role="presentation"';
+//If filter status is active set the class to active 
+//Default behavior index.php 
+if($filter['status'] == 'active') echo ' class="active"';
+echo '><a href="index.php">Active';
+echo '</a>';
+echo '</li>';
+//Display status if == $status
+foreach($directory->getStatuses() as $status){
+    echo '<li role ="presentation"';
+    if($filter['status'] == $status) echo ' class="active"';
+echo '><a href="index.php?status='.$status.'">';
+echo ucwords($status).'</a>';
+echo '</li>';
+}
+echo '</ul>';
+
 foreach ($directory->listings as $listing) {
     include 'views/list_item.php';
 }
