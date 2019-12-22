438d0edfd8c5ac2994557843bbb68ca37a209bd5
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-22 17:29:15 -0600

    Remove: Comment out the var_dump test Test: Add listing php[world] and view in browser

--- index.php
+++ index.php
@@ -13,13 +13,14 @@ $directory->selectListings($filter);
 
 $title = "PHP Conferences";
 require 'inc/header.php';
+/*
 $test = new ListingPremium(['description' => 'My description with 
 <b>Good</b> tags and <a href="http://example.com"> Bad Tags </a>']);
 var_dump($test);
 var_dump(get_class($test));
 var_dump(is_a($test, 'ListingBasic'));
 var_dump($test->getStatus());
-
+*/
 foreach ($directory->listings as $listing) {
     include 'views/list_item.php';
 }
